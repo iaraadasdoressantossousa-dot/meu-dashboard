0460a2f061f303fa68dd54a5219c6b33c61f6f09
iaraadasdoressantossousa-dot <iaraadasdoressantossousa@gmail.com>
2026-03-25 19:51:17 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -50,10 +50,10 @@ if uploaded_file is not None:
         # 6. Mostrar os dados calculados
         st.write("### Visualização dos Dados com ROI")
         if roi_medio > 50: {
-            st.subheader(label='Projeto viável')
+            st.subheader('Projeto viável')
         }
         else: {
-            st.subheader(label='Projeto não viável')
+            st.subheader('Projeto não viável')
         }
         st.dataframe(df)
     else:
