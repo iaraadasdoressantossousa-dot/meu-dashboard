7cfa51b01127780079479b1d3c1f1d9d1aeb7d61
iaraadasdoressantossousa-dot <iaraadasdoressantossousa@gmail.com>
2026-03-26 00:50:23 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -195,7 +195,7 @@ if uploaded_file is not None:
     else:
         st.error("Arquivo com colunas incorretas")
 
-else:
+ else:
     st.info("Envie um arquivo para começar")
 else:
     st.info("Aguardando upload do arquivo para processar os dados.")
