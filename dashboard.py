56e955208bd28c7c33d7e3430238b4d8cf8ad5e0
iaraadasdoressantossousa-dot <iaraadasdoressantossousa@gmail.com>
2026-03-26 13:32:06 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -58,7 +58,7 @@ if uploaded_file is not None:
 
         # --- PÁGINA: DASHBOARD ---
         if pagina == "📊 Dashboard":
-            st.markdown("<h1 style='text-align: center;'>DASHBOARD</h1>", unsafe_allow_html=True)
+            st.markdown("<h1 style='text-align: center;'>Dashboard</h1>", unsafe_allow_html=True)
             
             # Métricas por Ano em Colunas Dinâmicas
             if anos_selecionados:
